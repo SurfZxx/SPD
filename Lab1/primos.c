70ccf710935ce0f28e5bd64204f409893f63cdd2
SurfZxx <hugo.sousa.sa@hotmail.com>
2022-03-08 15:56:57 +0000

    Initial commit

---
+++ Lab1/primos.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define MAX_SIZE 1000
+
+int is_even(int number) {
+    if (number%2 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int multiple(int number) {
+    if (number%3 == 0) {
+        return 1;
+    } else if (number%5 == 0) {
+        return 1;
+    } else if (number%7 == 0) {
+        return 1;
+    } else if (number%9 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+void test(void) {
+    //int number;
+    //scanf("%d", &number);
+    int array[MAX_SIZE];
+    int elements = 0; //nr de elementos no array de primos
+    int i = 10; //vai incrementar ate MAX_SIZE
+    int result = 0;
+    for (int j = 0; j < 10; j++) {
+        result = is_even(j);
+        if (result == 0) {
+            array[elements] = j;
+            elements++;
+        }
+    }
+
+    while(i < MAX_SIZE) {
+        result = is_even(i);
+        if (result == 0) {
+            result = multiple(i);
+            if (result == 0) {
+                array[elements] = i;
+                elements++;
+            }
+        }
+        i++;
+    }
+
+    //int lenght = sizeof(array)/sizeof(array[0]);
+    for (int nr = 0; nr < elements; nr++) {
+        printf("%d ", array[nr]);
+    }
+
+}
+
+
+int main(int argc, char const *argv[]) {
+    test();
+    return 0;
+}
